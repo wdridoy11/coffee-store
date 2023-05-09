d6e204720b6d75e62daf04c9db3d79c34b00449c
Ridoy Sharif <developersridoy@gmail.com>
2023-05-09 17:14:29 +0600

    install react, react router, tailwincss, daisyui install complete

---
+++ src/components/coffee_card/CoffeeCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CoffeeCard = () => {
+  return <div>CoffeeCard</div>;
+};
+
+export default CoffeeCard;
---
+++ src/pages/create_coffee/CreateCoffee.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateCoffee = () => {
+  return <div>CreateCoffee</div>;
+};
+
+export default CreateCoffee;
